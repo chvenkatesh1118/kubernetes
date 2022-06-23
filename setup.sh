1deffc629b486e038091cdd10ae85b3d5aac8b08
Chanti <chvenkatesh.1118@gmail.com>
2022-06-23 14:47:43 +0530

    24-03

---
+++ setup.sh
@@ -0,0 +1,15 @@
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin
+kubectl version --short --client
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+eksctl create cluster --name chanti-cluster  \
+--region us-east-1 \
+--node-type t2.micro \
+--nodes-min 2 \
+--nodes-max 2 \
+--zones <AZ-1>,<AZ-2>
