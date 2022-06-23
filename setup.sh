85bc0454f46bfba6697580b4a2588b3283e4586a
Chanti <chvenkatesh.1118@gmail.com>
2022-06-23 15:28:22 +0530

    24-03

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
 eksctl create cluster --name chanti-cluster  \
---region us-east-1a \
+--region  us-east-1b \
 --node-type t2.micro \
 --nodes-min 2 \
 --nodes-max 2 \
